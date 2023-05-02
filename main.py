c7515f6ebe76902b2a52ef47a30262b6332b4146
Ali Shaikh <alihshaikh322@gmail.com>
2023-05-01 17:23:37 -0700

    added praw and sample program

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world!")
+import config
+import praw
+
+reddit = praw.Reddit(
+    client_id= config.clientID,
+    client_secret=config.clientSecret,
+    user_agent=config.clientSecret,
+)
+
+
+ucr_subreddit = reddit.subreddit("ucr")
+
+for post in ucr_subreddit.hot(limit=10):
+    print(post.title)
